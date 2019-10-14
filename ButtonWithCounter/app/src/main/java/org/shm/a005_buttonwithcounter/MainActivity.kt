92db4bb925d662db82ddd4a5c62df92f9c4bc952
Gülmen <42201841+ShmGulmen@users.noreply.github.com>
2019-10-14 16:08:15 +0300

    Button With Counter

---
+++ ButtonWithCounter/app/src/main/java/org/shm/a005_buttonwithcounter/MainActivity.kt
@@ -0,0 +1,42 @@
+package org.shm.a005_buttonwithcounter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var mCount = 0
+
+    private fun setCount()
+    {
+        MainActivityButtonCounter.text = (mCount++).toString()
+    }
+
+    private fun initViews()
+    {
+        setCount()
+    }
+
+    private fun init()
+    {
+        initViews()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        init()
+    }
+
+    fun onCounterButtonClicked(view: View)
+    {
+        setCount()
+    }
+
+    fun onExitButtonClicked(view: View)
+    {
+        finish()
+    }
+}
